e0c2a877fcfb711936e9d5f3254c819c4366093a
Hamid Ghorashi <hamidghorashi@hyperoffice.com>
2021-07-01 02:14:52 +0430

    Initialize UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-require('./bootstrap');
+import Vue from 'vue'
+import Home from './components/Home';
+import Clients from './components/Clients';
+import Stocks from './components/Stocks';
+import ClientsStocks from './components/ClientsStocks';
+
+require("./bootstrap");
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        Home,
+        Clients,
+        Stocks,
+        ClientsStocks
+    }
+});
---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script>window.Laravel = { csrf_token: "{{ csrf_token() }}" }</script>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <title>Stock Manager</title>
+    </head>
+
+    <body>
+        <section id="app">
+            <section class="container">
+                <article class="wrapper">
+                    <section class="row">
+                        <article class="col-md-6">
+                            <clients></clients>
+                        </article>
+                        <article class="col-md-6">
+                            <stocks></stocks>
+                        </article>
+                    </section>
+                </article>
+            </section>
+        </section>
+
+        <script src="{{ asset('js/app.js') }}"></script>        
+    </body>
+</html>
