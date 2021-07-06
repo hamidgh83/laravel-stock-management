eff0fef3cd58396524631b636f3183768bfd80b3
Hamid Ghorashi <hamidghorashi@hyperoffice.com>
2021-07-06 16:06:51 +0430

    Add store and Request service.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,13 @@ require("./bootstrap");
 
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import store from "./stores/user";
 import router from './router'
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    store
 });
